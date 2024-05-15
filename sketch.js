96b5eab67621f5d72590df3ceb59b7b998554145
EVAndradeSilva14 <emillymoon1910@gmail.com>
2024-05-15 12:07:30 -0300

    adicionando arquivos css e js

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(400, 400);
+    background("#FFFFFF");
+    
+  }
+  
+  function draw() {
+    
+    stroke("blue");
+    fill("#4CAF50");
+    
+    //console.log(mouseIsPressed);
+    
+    if(mouseIsPressed)(
+     rect(mouseX,mouseY,20,20)  
+    )
+    
+    
+  }
